fbdaead3caa36249054d06ca9ab9020536b3adad
Martin Synek <synekm@spstrutnov.cz>
2021-03-22 20:14:50 +0100

    Added an email database and database adding, removing and reporting to console code.

--- src/server.js
+++ src/server.js
@@ -1,18 +1,30 @@
 const path = require('path');
 const express = require('express');
 const jsondb = require('simple-json-db');
+
 const server = express();
+const database = new jsondb(path.join('..', 'data', 'emails.json'));
 
 const port = 8000;
 
 server.use(express.json());
 
 server.post('/prihlasit', (dotaz, odpoved) => {
-    console.log(dotaz.body);
+    if (database.get(dotaz.body.email) == undefined) {
+        database.set(dotaz.body.email, dotaz.body.email);
+        console.log(`Prihlasuji email ${dotaz.body.email}!`)
+    } else {
+        console.log(`${dotaz.body.email} je jiz prihlasen.`)
+    }
 });
 
 server.post('/odhlasit', (dotaz, odpoved) => {
-    console.log(dotaz.body);
+    if (database.get(dotaz.body.email) == undefined) {
+        console.log(`${dotaz.body.email} je jiz odhlasen.`)
+    } else {
+        database.delete(dotaz.body.email);
+        console.log(`Odhlasuji email ${dotaz.body.email}!`)
+    }
 });
 
 server.use(express.static(path.join(__dirname, 'www')));
